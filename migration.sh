158af507148e86bbc0d5f738586be8570eac5d20
Amney El Azarifi <amneyazarifi@gmail.com>
2024-09-23 20:02:19 +0100

    Changed from docker backup to linux localhost server backup.

--- migration.sh
+++ migration.sh
@@ -32,18 +32,40 @@ Please follow the instructions to help guide you through this process.
 
 EOF
 
-# MySQL Dump of database
-echo "Creating a backup of your database container:"
+# ssh into server
+# Not necessary if doing migration inside server
 
-read -p "Enter container name [default=production-mysql-1]: " CONTAINER_NAME
-CONTAINER_NAME=${CONTAINER_NAME:-production-mysql-1}
-read -p "Enter database user [default=root]: " DB_USER
+# MySQL Dump of database
+echo "Creating a backup of your database:"
+
+read -p "Enter database user [default=openemr]: " DB_USER
 DB_USER=${DB_USER:-root}
-read -p "Enter database password [default=root]: " DB_PASSWORD
+read -p "Enter database password [default=openemr]: " DB_PASSWORD
 DB_PASSWORD=${DB_PASSWORD:-root}
 
-# docker exec $CONTAINER_NAME /usr/bin/mysqldump --all-databases -u $DB_USER --password=$DB_PASSWORD > backup_db.sql
+# Backup database
+mysqldump openemr --opt --quote-names --hex-blob -u $DB_USER --password=$DB_PASSWORD | gzip > openemr.sql.gz
 
-# ToDo (Consider Percona XtraBackup. For large databases, such as those larger than 100 GB, this can provide a significantly 
+# Backup filesystem
+# Refer to your OS and webserver documentation to obtain your webserver root directory. For example, Ubuntu's webserver root 
+# directory is /var/www/html 
+echo "Creating a backup of your openemr filesystem:"
+
+read -p "Enter the path to your openemr filesystem [default=/var/www/html/openemr/]: " OPENEMR_PATH
+OPENEMR_PATH=${OPENEMR_PATH:-"/var/www/html/openemr/"}
+
+# tar -C $OPENEMR_PATH -zcf openemr.tar.gz ./
+sudo tar -C $OPENEMR_PATH -zcf openemr.tar.gz ./ # 
+
+# Combine the database and backups together
+tar -cvf emr_backup.tar openemr.sql.gz openemr.tar.gz
+
+
+
+# This page lists which files to backup and how to recover
+# See https://www.open-emr.org/wiki/index.php/Backing_Up_Using_the_Built_In_Tools
+# OPTIONAL
+# Consider Percona XtraBackup. For large databases, such as those larger than 100 GB, this can provide a significantly
 # better restoration time than some other tools.)
 # See https://docs.aws.amazon.com/prescriptive-guidance/latest/migration-large-mysql-mariadb-databases/percona-xtrabackup.html
+# https://github.com/openemr/openemr-devops/blob/master/docker/mysql-xtrabackup/xbackup-wrapper.sh
